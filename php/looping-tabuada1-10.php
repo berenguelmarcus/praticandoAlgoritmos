84d83b52b99d22f12e62acc2a4ee511edfc36a92
Marcus V B Reis <marcus_berenguel@hotmail.com>
2020-10-26 02:15:40 -0300

    tabuada do 1 ao 10, usando estrutura de repetição em php

---
+++ php/looping-tabuada1-10.php
@@ -0,0 +1,7 @@
+<?php
+	for ($i = 0; $i <= 10; $i++) {
+		for ($multiplicador = 0; $multiplicador <= 10; 		 $multiplicador++) {
+			echo $i, " x ", $multiplicador, " = ", $i * $multiplicador, "\n";
+		}
+	}
+?>
