3bef10bd2ae382e08ac6292380378ba755ffb6d1
Marcus V B Reis <marcus_berenguel@hotmail.com>
2020-10-26 03:09:44 -0300

    uma array com outra array em seu interior

---
+++ php/array_multidimensionais.php
@@ -0,0 +1,10 @@
+<?php
+$nomes = array(
+  10 => "Pedro",
+  "dev" => "Antônia",
+  3.1415 => "Beto",
+  true => "Julia",
+  "grupo" => array("Ana", "Lucas", "José", "Mateus")
+);
+echo $nomes["grupo"];   //imprime todo o grupo
+?>
